0e62cf3524f94675cf8525460790b7b684d6a6fa
asilbeksagdullayev <asilbek1188akeng@gmail.com>
2023-10-20 20:33:43 +0500

    second_codes

--- src/start_pice.ts
+++ src/start_pice.ts
@@ -1,57 +1,74 @@
 import * as yup from "yup";
 
-const userInput:HTMLInputElement = document.querySelector(".first")!;
-const error1:HTMLParagraphElement = document.querySelector(".error1")!;
-const btn:HTMLButtonElement = document.querySelector(".btn")!;
+const first_page: HTMLDivElement = document.querySelector(".container")!;
+const second_page: HTMLDivElement = document.querySelector(".second_page")!;
+const userInput: HTMLInputElement = document.querySelector(".first")!;
+const cardInput: HTMLInputElement = document.querySelector(".secondmn")!;
+const error1: HTMLParagraphElement = document.querySelector(".error1")!;
+const error2: HTMLParagraphElement = document.querySelector(".second")!;
+const btn: HTMLButtonElement = document.querySelector(".btn")!;
 
-
+let inputMessage: string = ""; // Declare the variables outside the event listener function
+let cardMessage: string = "";
 
 const schema = yup.object().shape({
- input: yup.number().required(),
-// userName: yup.string(),
-// email: yup.string().email().required(),
-// password: yup.string().required().min(8),
+  input: yup.number().required(),
+  inputSecond: yup.number().required(),
+  // userName: yup.string(),
+  // email: yup.string().email().required(),
+  // password: yup.string().required().min(8),
 });
 
 btn.addEventListener("click", () => {
- const formData = {
-// email: emailInput.value,
-//    password: passwordInput.value,
-input:userInput.value
- };
- schema
- .validate(formData)
- .then(() => {
-   // console.log(formData);
-   // error1.innerText = "Please wait...";
-   // stop.style.display ="block";
-   // emailInput.value = "";
-   // passwordInput.value = "";
-   // setTimeout(() => {
-   //   stop.style.display ="none";
-   //   second_page.style.display ='block';
-   // }, 3000);
+  const formData = {
+    input: userInput.value,
+    inputSecond: cardInput.value,
+  };
 
+  schema
+    .validate(formData)
+    .then(() => {
+      console.log(formData);
+      // central();
 
- })
- ///Al_Dicooo
- .catch((error) => {
-   userInput.value ="";
-   error1.innerText = "Error";
-   error1.style.display = "block";
+    })
+    .catch((error) => {
+      userInput.value = "";
+      cardInput.value = "";
+      error1.innerText = "Error";
+      error2.innerText = "Error";
+      error1.style.display = "block";
+      error2.style.display = "block";
 
+      setTimeout(() => {
+        error1.style.display = "none";
+        error2.style.display = "none";
+      }, 2000);
 
-   setTimeout(() => {
-       error1.style.display = "none";
+      console.log(error);
 
-
-
-     }, 2000);
-     
-
-     console.log(error);
-
-     // const hacer = formData;
 
     });
-   });
+
+  inputMessage = formData.input; // Assign the values to the variables
+  cardMessage = formData.inputSecond;
+});
+
+// function central() {
+//   first_page.style.display = "none";
+// }
+
+// function create_container() {
+//   const table = document.createElement('div');
+//   for (let i = 0; i < inputMessage; i++) {
+//     const row = document.createElement('div');
+
+//     for (let j = 0; j < cardMessage; j++) {
+//       const cell = document.createElement('div');
+//       // Set the content of each cell, e.g., cell.textContent = 'Cell Content';
+//       row.appendChild(cell);
+//     }
+
+//     table.appendChild(row);
+//   }
+// }
